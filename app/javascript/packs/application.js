587d647bfd52d552a719657f77a7e9869ec85154
bashforger <bash.forger@gmail.com>
2021-02-02 17:48:42 +0500

    Initialized Bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,10 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+// Bootstrap Initialization
+//= require jquery3
+//= require popper
+//= require bootstrap
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
