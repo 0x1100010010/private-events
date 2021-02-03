c9dded139cd43c0f51455e31de576c3b0630a034
bashforger <bash.forger@gmail.com>
2021-02-03 14:17:59 +0500

    Added Sessions controller, Header style

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,7 @@
 class SessionsController < ApplicationController
+  def signup
+  end
+
   def signin
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'sessions/signup'
   get 'sessions/signin'
   get 'sessions/signout'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'users#index'
 end
