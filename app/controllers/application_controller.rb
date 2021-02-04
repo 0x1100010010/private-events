6b0ef5cd8b74b3df14bc684a2c9a5e8e353f5cc7
bashforger <bash.forger@gmail.com>
2021-02-04 19:07:12 +0500

    Modified application_controller to encapsolate helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user
-
-  def current_user
-    if session[:user_id]
-      @current_user ||= User.find(session[:user_id])
-    else
-      @current_user = nil
-    end
-  end
+  include ApplicationHelper
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,16 @@
 module ApplicationHelper
 
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+    else
+      @current_user = nil
+    end
+  end
+
   def require_session
     unless current_user
-      redirect_to login_path, :alert => 'Sign up or Log in to access this feature!'
+      redirect_to sessions_signin_path, :alert => 'Sign up or Log in to access this feature!'
     end
   end
 
@@ -10,7 +18,7 @@ module ApplicationHelper
     html_out = ''
     if session[:user_id]
       html_out << "<li class=\"nav-item\"><%= link_to 'Sign Out', sessions_signout_path, class: 'nav-link' %></li>"
-      html_out << "<li class=\"nav-item\"><%= link_to current_user.username, root_path , class: 'nav-link' %></li>"
+      html_out << "<li class=\"nav-item\"><%= link_to @current_user.username, root_path , class: 'nav-link' %></li>" if @current_user
     else
       html_out << "<li class=\"nav-item\"><%= link_to 'Sign In', sessions_signin_path, class: 'nav-link' %></li>"
       html_out << "<li class=\"nav-item\"><%= link_to 'Sign Up', sessions_signup_path, class: 'nav-link' %></li>"
