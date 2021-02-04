a3a2d9493c1f01b3bf8efae95e6d4767d04af3b9
bashforger <bash.forger@gmail.com>
2021-02-04 18:00:15 +0500

    Migrated user_id to events table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_04_111409) do
+ActiveRecord::Schema.define(version: 2021_02_04_124703) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_02_04_111409) do
     t.date "date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
