3a2a57c607114419996e0d956a7dc147dcf88dbb
bashforger <bash.forger@gmail.com>
2021-02-04 15:05:28 +0500

    Added Sessions Create/Destroy Functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,22 @@
 class SessionsController < ApplicationController
-  def signup
+  # before_action :set_user, only: %i[ create destroy ]
+
+  def new
   end
 
-  def signin
+  def create
+    @user = User.find_by_username(params[:username])
+    if @user
+      session[:user_id] = @user.id
+      redirect_to root_url, notice: "Signed in successfully!"
+    else
+      flash.now[:alert] = "Username is invalid"
+    end
   end
 
-  def signout
+  def destroy
+    reset_session
+    redirect_to root_url, notice: "Signed out successfully!"
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'sessions/signup'
-  get 'sessions/signin'
-  get 'sessions/signout'
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'sessions/signup', to: 'users#new'
+  get 'sessions/signin', to: 'sessions#new'
+  get 'sessions/signout', to: 'sessions#destroy'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'users#index'
