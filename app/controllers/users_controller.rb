03d3cbab6415ea99a19b636905cbceeeca99ce6c
bashforger <bash.forger@gmail.com>
2021-02-04 15:06:24 +0500

    Added Session Start On Sign Up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  include UsersHelper
   before_action :set_user, only: %i[ show edit update destroy ]
 
   # GET /users or /users.json
@@ -22,12 +23,11 @@ class UsersController < ApplicationController
   # POST /users or /users.json
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: "User was successfully created." }
-        format.json { render :show, status: :created, location: @user }
-      else
+    
+    if @user.save
+      start_session(@user)
+    else
+      respond_to do |format|
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
@@ -56,14 +56,14 @@ class UsersController < ApplicationController
     end
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
+  def signin
+    @user = User.find_by_username(params[:username])
+    if @user
+      session[:user_id] = @user.id
+      redirect_to root_url, notice: "Signed in successfully!"
+    else
+      flash.now[:alert] = "Username is invalid"
     end
+  end
 
-    # Only allow a list of trusted parameters through.
-    def user_params
-      params.require(:user).permit(:name, :username)
-    end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,21 @@
 module UsersHelper
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def user_params
+    params.require(:user).permit(:name, :username)
+  end
+
+  def start_session(user)
+    if user
+      session[:user_id] = user.id
+      redirect_to root_url, notice: "User #{user.name} account signed up successfully!"
+      # flash.now[:notice] = "User #{user.name} account created and Signed in successfully!"
+    else
+      flash.now[:alert] = "Username is invalid"
+    end
+  end
 end
