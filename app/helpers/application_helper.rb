a6119de39cb5d85280d1715a383f95741ed757b7
bashforger <bash.forger@gmail.com>
2021-02-04 15:00:37 +0500

    Added Header helper in ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def navbar_links
+    html_out = ''
+    if session[:user_id]
+      html_out << "<li class=\"nav-item\"><%= link_to 'Sign Out', sessions_signout_path, class: 'nav-link' %></li>"
+      html_out << "<li class=\"nav-item\"><%= link_to current_user.username, root_path , class: 'nav-link' %></li>"
+    else
+      html_out << "<li class=\"nav-item\"><%= link_to 'Sign In', sessions_signin_path, class: 'nav-link' %></li>"
+      html_out << "<li class=\"nav-item\"><%= link_to 'Sign Up', sessions_signup_path, class: 'nav-link' %></li>"
+    end
+    render inline: html_out
+  end
 end
