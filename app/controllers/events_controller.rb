b1dd86bcbf5f4c78ace716987115d7c9fcbc55f2
bashforger <bash.forger@gmail.com>
2021-02-05 16:11:06 +0500

    Added access control based editing, refactored code

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,7 @@
 class EventsController < ApplicationController
   include EventsHelper
   before_action :require_session, except: [:index]
+  before_action :creator?, only: %i[edit destroy]
 
   # GET /events or /events.json
   def index
@@ -9,6 +10,7 @@ class EventsController < ApplicationController
 
   # GET /events/1 or /events/1.json
   def show
+    set_event
   end
 
   # GET /events/new
@@ -18,23 +20,25 @@ class EventsController < ApplicationController
 
   # GET /events/1/edit
   def edit
+    set_event
   end
 
   # POST /events or /events.json
   def create
     @event = current_user.events.build(event_params)
 
-      if @event.save
-        flash[:success] = "Event '#{@event.name}' created!"
-        redirect_to @event
-      else
-        flash[:alert] = @event.errors
-        render 'new'
-      end
+    if @event.save
+      flash[:success] = "Event '#{@event.name}' created!"
+      redirect_to root_path
+    else
+      flash[:alert] = @event.errors
+      render 'new'
+    end
   end
 
   # PATCH/PUT /events/1 or /events/1.json
   def update
+    set_event
     respond_to do |format|
       if @event.update(event_params)
         format.html { redirect_to @event, notice: "Event was successfully updated." }
--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -7,4 +7,28 @@ module EventsHelper
   def event_params
     params.require(:event).permit(:name, :description, :location, :start_time, :end_time, :date, :user_id)
   end
+
+  def render_controls(event)
+    if current_user && event.creator.id == current_user.id
+      @@html_out = ''
+      @@html_out << "<%= link_to event do %>
+          <i class=\"fas fa-link\"></i>
+        <% end %>
+        <%= link_to edit_event_path(event) do %>
+          <i class=\"far fa-edit\"></i>
+        <% end %>
+        <%= link_to event, method: :delete, data: { confirm: 'Are you sure?' } do %>
+          <i class=\"fas fa-trash\"></i>
+        <% end %>"
+      render inline: @@html_out,locals: { event: event }
+    end
+  end
+
+  def creator?
+    set_event
+    return if current_user.id == @event.user_id
+    flash[:alert] = 'Unauthorized request'
+    redirect_to root_path
+  end
+
 end
