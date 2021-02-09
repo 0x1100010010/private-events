25d70e60f14190a3cfa5684a901cf60c1de66312
bashforger <bash.forger@gmail.com>
2021-02-09 17:56:59 +0500

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,10 @@ Rails.application.routes.draw do
   end
 
   resources :users
+  get 'invite', to: 'users#invitation'
   resources :events
+  resources :event_enrolment, only: [:create, :destroy, :update]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'events#index'
 end
