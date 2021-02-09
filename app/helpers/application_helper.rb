b9ee5af574770200e6406c099f81c49bf925890c
bashforger <bash.forger@gmail.com>
2021-02-09 17:52:57 +0500

    Modified Header & Footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,7 +18,8 @@ module ApplicationHelper
     html_out = ''
     if session[:user_id]
       html_out << "<li class=\"nav-item\"><%= link_to 'Sign Out', signout_path, class: 'nav-link' %></li>"
-      html_out << "<li class=\"nav-item\"><%= link_to @current_user.username, root_path , class: 'nav-link' %></li>" if @current_user
+      html_out << "<%= link_to 'Create Event', new_event_path, class: 'nav-link' %>"
+      html_out << "<li class=\"nav-item\"><%= link_to @current_user.username, user_path(current_user) , class: 'nav-link' %></li>" if @current_user
     else
       html_out << "<li class=\"nav-item\"><%= link_to 'Sign In', signin_path, class: 'nav-link' %></li>"
       html_out << "<li class=\"nav-item\"><%= link_to 'Sign Up', signup_path, class: 'nav-link' %></li>"
